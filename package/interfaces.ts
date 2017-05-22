f36c14857ffa6823eb5a1f795a1acd5458231666
navix <inavix@gmail.com>
2017-05-22 21:06:12 +0300

    State styling

--- package/interfaces.ts
+++ package/interfaces.ts
@@ -1,3 +1,10 @@
 import { types } from 'typestyle';
 
 export type Style = types.NestedCSSProperties;
+
+export interface StylerState {
+  name: string;
+  value: StylerStateValue;
+}
+
+export type StylerStateValue = string | number | null;
--- package/styler.service.ts
+++ package/styler.service.ts
@@ -1,24 +1,67 @@
 import { Injectable } from '@angular/core';
 import { style as s } from 'typestyle';
+import { StylerState, StylerStateValue } from './interfaces';
+
+/**
+ * @todo interfaces!!
+ * @todo add (!replace) styles on additional registrations (or add upgrade methods)
+ * @todo cache
+ */
 
 @Injectable()
 export class StylerService {
 
   styles: any;
+  statesStyles: any = {};
+  states: StylerState[] = [];
 
-  register(styles: any) {
+  register(styles: any): void {
     this.styles = styles;
   }
 
-  getHostClass() {
+  registerState(name: string, styles: any, defaultState: StylerStateValue = null): void {
+    this.states.push({name, value: defaultState});
+    this.statesStyles[name] = styles;
+  }
+
+  getHostClass(): string {
     return this.getByName('host');
   }
 
-  getByName(name: any) {
+  getByName(name: string): string {
     if (this.styles[name]) {
-      return s(this.styles[name]);
+      // get base styles
+      const base = this.styles[name];
+      // merge with state styles
+      const compiled = this.states
+          .map(s => this.getStateStyles(s.name, s.value))
+          .map(styles => (styles && styles[name]) || {})
+          .reduce((c, s) => {
+            return {...c, ...s};
+          }, base);
+      // get class name (by typestyle)
+      return s(compiled);
     } else {
       throw new Error(`Styler: component does not have "${name}" property!`);
+    }
+  }
+
+  getStateStyles(name: string, value: StylerStateValue): any {
+    if (value !== null) {
+      if (this.statesStyles[name]) {
+        return this.statesStyles[name][value] || {};
+      } else {
+        throw new Error(`Styler: styles for state "${name}" not registered!`);
+      }
+    }
+  }
+
+  setState(name: string, value: StylerStateValue): void {
+    const state = this.states.find(s => s.name === name);
+    if (state) {
+      state.value = value;
+    } else {
+      throw new Error(`Styler: state "${name} not registered!`);
     }
   }
 

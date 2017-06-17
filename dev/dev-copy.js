8391d8bbfd096e3a8c7e280460e52acd2914aade
navix <inavix@gmail.com>
2017-06-17 15:03:16 +0300

    build: dev-copy script

---
+++ dev/dev-copy.js
@@ -0,0 +1,6 @@
+const path = require('path');
+const fs = require('fs-extra');
+
+const config = require('./dev-copy-config.json');
+
+fs.copySync(path.resolve('dist/package'), path.resolve(config.path));
